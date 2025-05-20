db46cb808ba0fedd066cc633f3ccf41344dca2a6
Guillermo-UD <a24216353@alumnos.uady.mx>
2025-05-19 22:23:42 -0600

    Subi la funcion escribir_archivo

--- boveda personal.c
+++ boveda personal.c
@@ -149,6 +149,51 @@ void crear_archivo(){
 }
 
 /*
+Nombre: escribir_archivo
+Objetivo: Función para escribir en un archivo.
+Descripción: Es el proceso para escribir en un archivo inmediatamente después de crearlo.
+*/
+void escribir_archivo(const char *nombre_archivo){
+	char linea[256];
+	FILE *archivo = fopen(nombre_archivo, "w");
+	// Proceso
+	if (archivo == NULL) {
+		// Salida
+		perror("Error al abrir el archivo");
+		printf("Presiona Enter para continuar...");
+		getchar();
+		system("cls");
+		
+		return;
+	}
+	
+	printf("--- Importante ---\n");
+	printf("Escribe lo que quieras.\n");
+	printf("Cuando quieras cambiar de línea presiona Enter.\n");
+	printf("Para salir, en una nueva línea escribe 'salir' y presiona Enter\n");
+	printf("Contenido del archivo:\n");
+	
+	while (1) {
+		fgets(linea, sizeof(linea), stdin);
+		linea[strcspn(linea, "\n")] = '\0'; // Elimina el salto de línea
+		
+		if (strcmp(linea, "salir") == 0) { // Si el usuario escribe "salir", termina
+			break;
+		}
+		
+		fprintf(archivo, "%s\n", linea);
+	}
+	
+	fclose(archivo);
+	system("cls");
+	printf("El archivo '%s' se ha guardado correctamente.\n", nombre_archivo);
+	printf("Presiona Enter para continuar...");
+	getchar();
+	system("cls");
+	
+	return;
+}
+/*
 Nombre: descifrar_archivo
 Objetivo:Función para el descifrado de archivos txt.
 Descripción:Es el proceso para aplicar el descifrado XOR en un archivo origen a uno nuevo.
